d6819cf799f54fee0d5282c835ac6ba0bfde7451
Alfred Qiu <sc941203@gmail.com>
2017-07-08 20:40:09 +0800

    modify

--- src/createApp.js
+++ src/createApp.js
@@ -5,13 +5,37 @@ import subscribe from "./createStore"
 
 function createApp(){
   let store = createStore();
-  
-  function model(name){
-  	subscribe(name,store);
+
+
+  /**
+   * 数据模型model订阅stroe
+   * @param  {[type]} names [description]
+   * @return {[type]}       [description]
+   */
+  function subscribeStore(names){
+	if ( Array.isArray(names) ){
+	  names.map(name => {
+  	    subscribe(name,store);
+      });
+    } else if ( typeof names === "string" ){
+	  subscribe(names,store);
+    };
+  };
+
+  /**
+   * 数据模型model订阅stroe
+   * @param  {[type]} names [description]
+   * @return {[type]}       [description]
+   */
+  function model(names){
+    if ( typeof names === "function" ){
+	  names = names();
+    };
+
+    subscribeStore(names);
   };
 
   return {
-  	model
+  	model: 
   };
-
 };
